b68ee2c1d6f02d493b9e635a8084e539e4462f93
afiq tokayahibu <afiq.samsudin.18@ucl.ac.uk>
2021-03-12 18:25:53 +0000

    fixed bugs

--- src/components/pageBar/index.js
+++ src/components/pageBar/index.js
@@ -4,34 +4,50 @@ import {Pagination} from 'react-bootstrap';
 export default class PageBar extends React.Component{
     constructor(props){
         super(props)
-        this.state = {
-            page: props.page,
-            totalPage: props.totalPage,
-        }
+
     }
 
     firstPage(){
-        if(parseInt(this.state.page) === 1){
+        if(parseInt(this.props.page) === 1){
             return true
         }
         return false
     }
 
     lastPage(){
-        if(this.state.page == this.state.totalPage){
+        if(this.props.page >= this.props.totalPage){
             return true
         }
         return false
     }
 
+    forward(){
+        if (this.props.totalPage == 0){
+            this.props.getPageData.bind(this,1)
+        }
+        else{
+            this.props.getPageData.bind(this,this.props.totalPage)
+        }
+    }
+
+    displaylast(){
+        if (this.props.totalPage > 3){
+            return <Pagination.Item key={this.props.totalPage} onClick={this.props.getPageData.bind(this,this.props.totalPage)}>{this.props.totalPage}</Pagination.Item>
+        }
+    }
+
 
     getpage = () => {
-        const maxpage = parseInt(this.state.totalPage)
-        const currentpage = parseInt(this.state.page)
+        const maxpage = parseInt(this.props.totalPage)
+        const currentpage = parseInt(this.props.page)
 
         let pages = []
-
-        if (currentpage == 1 || currentpage == 2 || currentpage == 3){
+        if (maxpage < 3){
+            for (let i = 1; i <= maxpage; i++){
+                pages.push(<Pagination.Item key={i} onClick={this.props.getPageData.bind(this,i)}>{i}</Pagination.Item>)
+            }
+        }
+        else if (currentpage == 1 || currentpage == 2 || currentpage == 3){
             for (let i = 2; i < currentpage + 2; i++){
                 pages.push(<Pagination.Item key={i} onClick={this.props.getPageData.bind(this,i)}>{i}</Pagination.Item>)
             }
@@ -54,16 +70,17 @@ export default class PageBar extends React.Component{
     }
 
     render(){
+        console.log(this.props.totalPage)
         return(
         <>
         <Pagination>
             <Pagination.First key='firstpage' onClick={this.props.getPageData.bind(this,1)}/>
-            <Pagination.Prev  key='prevpage' disabled={this.firstPage(this)} onClick={this.props.getPageData.bind(this,parseInt(this.state.page) - 1)} />
+            <Pagination.Prev  key='prevpage' disabled={this.firstPage(this)} onClick={this.props.getPageData.bind(this,parseInt(this.props.page) - 1)} />
             <Pagination.Item key={1} onClick={this.props.getPageData.bind(this,1)}>{1}</Pagination.Item>
             {this.getpage()}
-            <Pagination.Item key={this.state.totalPage} onClick={this.props.getPageData.bind(this,this.state.totalPage)}>{this.state.totalPage}</Pagination.Item>
-            <Pagination.Next key='nextpage' disabled={this.lastPage(this)} onClick={this.props.getPageData.bind(this,parseInt(this.state.page) + 1)}/>
-            <Pagination.Last key='lastpage' onClick={this.props.getPageData.bind(this,this.state.totalPage)}/>
+            {this.displaylast()}
+            <Pagination.Next key='nextpage' disabled={this.lastPage(this)} onClick={this.props.getPageData.bind(this,parseInt(this.props.page) + 1)}/>
+            <Pagination.Last key='lastpage' onClick={this.forward.bind(this)}/>
         </Pagination>
         </>
         )
